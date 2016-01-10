d2cc3ebf697b1a69de77c68b224a145e377ba7b4
Tuesday-Last <sgnortz@gmail>
2016-01-10 15:11:57 -0800

    Seperated index.html into a .js file and updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-201 Guessing Game,
-simple guessing game with 4 questions coded to respond if answer is correct or incorrect and the last question responds with if the wrong answer is to high or to low. 
+## 201 Guessing Game
+A simple guessing game with four questions coded to respond if answer is correct or incorrect and the last question responds with if the wrong answer is to high or to low.
+
+Inspired by Monty Python's Holy Grail skit
---
+++ game.js
@@ -0,0 +1,67 @@
+alert("Halt! Before ye enter this page ye must answer me these questions three.");
+var question1 = prompt("One, Who is he who crafted me?").toLowerCase();
+
+	console.log("Answer to question1 " + question1);
+
+	if (question1 === "sam nortz") {
+		console.log("question1 correct");
+		alert("Yess, correct he is the one that crafted me");
+	} else if (question1 === "sam") {
+		console.log("question1 correct, short");
+		alert("Correct he is the one that crafted me");
+	} else {
+		console.log("incorrect name");
+		alert("WRONG!");
+	};
+
+var question2 = prompt("What is his quest?").toLowerCase();
+
+	console.log("Answer to question2 " + question2);
+
+	if (question2 === "to learn how to code") {
+		console.log("question 2 correct");
+		alert("Yess, correct he seeks the coding secretsss");
+	} else if (question2 === "to code") {
+		console.log("question 2 correct, short")
+		alert("Very Well....acceptable");
+	} else {
+		console.log("question 2 incorrect");
+		alert("WRONG! Why is he taking this class? Think!");
+	};
+
+var question3 = prompt("What is his favorite color?").toLowerCase();
+
+	console.log("Answer to question 3 " + question3);
+
+	if (question3 === "blue") {
+		console.log("question 3 correct");
+		alert("Yess, correct he does like blue");
+	} else {
+		console.log("question 3 incorrect");
+		alert("WRONG! THAT IS NOT MY FAVORITE COLOR!");
+	};
+
+var bonusQuestion = prompt("Bonus: What is the airspeed velocity of an unladen swallow? (in mph)");
+	var airSpeed = parseInt(bonusQuestion);
+	console.log("air speed guess " + airSpeed);
+
+	if (airSpeed === 24) {
+		console.log("bonus question correct");
+		alert("Correct you may proceed");
+	} else if (bonusQuestion === "african or european swallow?") {
+		console.log("OH NO!")
+		alert("I, I don't know tha- AAAHHHHHHH!");
+	} else if (airSpeed > 24) {
+		console.log("bonusQuestion to high");
+		alert("WRONG! AHAHHA HA! to fast");
+	}
+	else if (airSpeed < 24) {
+		console.log("bonusQuestion to low");
+		alert("WRONG! AHAHHA HA! to slow!");
+	}
+	else {
+		alert("what was that? WRONG!");
+		console.log(bonusQuestion + " not a number");
+	};
+
+
