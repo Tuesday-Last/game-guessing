54f6825874df796ba46e0eb63d4f0d9296a7e73c
Stephanie <lopezlstephanie@gmail.com>
2016-01-17 12:14:55 -0800

    Added images to go along with responses and styling via a css file.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## 201 Guessing Game
-A simple guessing game with four questions coded to respond if answer is correct or incorrect and the last question responds with if the wrong answer is to high or to low.
+- A simple guessing game with four questions coded to respond if answer is correct or incorrect and the last question responds with images.
+- Added images and style elements via a css file.
+- Is working just fine.  Don't mess with it. 
 
 Inspired by Monty Python's Holy Grail skit
--- game.js
+++ game.js
@@ -6,26 +6,36 @@ var ans3 = document.getElementById('a3');
 var bonus = document.getElementById('bonus');
 var correctCount = document.getElementById('elCorrect');
 
-var questions = ["One, Who is he who crafted me?", "What is his quest?", "What is his favorite color?", "Bonus: What is the airspeed velocity of an unladen swallow? (in mph)"];
-var answers = ["Yess, correct he is the one that crafted me", "Yess, correct he seeks the coding secretsss", "Yess, correct he does like blue", 24];
-var els = [ans1, ans2, ans3, bonus]
+var img1 = document.getElementById('im1');
+var img2 = document.getElementById('im2');
+var img3 = document.getElementById('im3');
+var img4 = document.getElementById('im4');
 
-function game(questions, answers, index) {
+
+var questions = ["One, Who is he who crafted me?", "What is his quest?", "What is his favorite color?", "Bonus: What is the airspeed velocity of an unladen swallow? (in mph)"];
+var answers = ["sam", "to code", "blue", 24];
+var els = [ans1, ans2, ans3, bonus];
+
+var imgs = [img1, img2, img3, img4];
+
+function game(questions, answers, index, images) {
 	var ques = prompt(questions).toLowerCase();
 
-	if (ques === answers || userInput === parseInt(answers)) {
+	if (ques === answers || ques === parseInt(answers)) {
 		console.log(ques + ' correct')
-		els[index].textContent = userInput + ' is correct';
+		els[index].textContent = ques + ' is correct';
+		imgs[images].innerHTML = '<img src = "images/rightanswer.jpg" width="150" height="100">';
 		numCorrect++
 	}
 	 	else {
 	 		console.log(ques + ' incorrect')
-			els[index].textContent = userInput + ' is incorrect';
+			els[index].textContent = ques + ' is incorrect';
+			imgs[images].innerHTML = '<img src = "images/wrong-answer.png" width="150" height="100">';
 	 	}
 }
 
 for (var i = 0; i < questions.length; i++) {
- game(questions[i], answers[i], i);
+ game(questions[i], answers[i], i, i);
 }
 
 elCorrect.textContent = "You got " + numCorrect + " out of 3 questions and the bonus question!"
